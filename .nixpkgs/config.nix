d7b8dff9b9c8b938dfcab4217c0f54cd8b78e574
Tom Hinton <tom.hinton@cse.org.uk>
2016-06-28 09:39:40 +0100

    fix missing bits of session

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -3,7 +3,7 @@
 
    packageOverrides = super: let self=super.pkgs; in rec {
      dmenu = super.dmenu.override {
-       patches = [ ../../patches/dmenu-number-output.patch ];
+       patches = [ ./patches/dmenu-number-output.patch ];
      };
      #emacs24 has a silly bug with xft fonts.
      emacs = super.lib.overrideDerivation
@@ -11,8 +11,8 @@
          withGTK3 = false;
          withGTK2 = false;
          })
-       (attrs : { patches = super.emacs.patches ++ [ ../../patches/emacs-xft.patch ]; }) ;
+       (attrs : { patches = super.emacs.patches ++ [ ./patches/emacs-xft.patch ]; }) ;
 
-     epass = import ../../epass;
+     epass = import ./epass;
    };
 }
