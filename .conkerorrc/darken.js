11e346b96d47b1f7f1c4e8274e29fbaf96d46b3b
Tom Hinton <tom.hinton@cse.org.uk>
2017-02-28 15:15:27 +0000

    conkeror fixes

--- .conkerorrc/darken.js
+++ .conkerorrc/darken.js
@@ -1,15 +1,17 @@
-function darken_page (I) {
-    var styles='* { background: #3f3f3f !important; color: #d5d2be !important; }'+
-        ':link, :link * { color: #aaccff !important; }'+
-        ':visited, :visited * { color: #d75047 !important; }';
-    var document = I.buffer.document;
-    var newSS=document.createElement('link');
-    newSS.rel='stylesheet';
-    newSS.href='data:text/css,'+escape(styles);
-    document.getElementsByTagName("head")[0].appendChild(newSS);
+var dark_stylesheet = make_css_data_uri(
+    ['* { background: #3f3f3f !important; color: #d5d2be !important; }'
+     , ':link, :link * { color: #aaccff !important; }'
+     , ':visited, :visited * { color: #d75047 !important; }'] );
+
+var dark_enabled = false;
+
+function toggle_dark_mode (I) {
+    if (dark_enabled = !dark_enabled) {
+        register_user_stylesheet(dark_stylesheet);
+    } else {
+        unregister_user_stylesheet(dark_stylesheet);
+    }
 }
 
-interactive("darken-page", "Darken the page in an attempt to save your eyes.",
-            darken_page);
-
-define_key(content_buffer_normal_keymap, "C-d", "darken-page");
+interactive("toggle-dark-mode", "Darken the page, or undarken it", toggle_dark_mode);
+define_key(content_buffer_normal_keymap, "C-d", "toggle-dark-mode");
