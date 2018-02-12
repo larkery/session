0a05d8bf3ec35203adcd44e9ed03f8ea1b95a45d
Tom Hinton <t@larkery.com>
2018-02-12 16:14:05 +0000

    compton

--- .config/nixpkgs/overlays/changes/default.nix
+++ .config/nixpkgs/overlays/changes/default.nix
@@ -2,22 +2,30 @@ self: super:
 
 {
   emacs25 =
-    let
-    patch0 = ./patches/0001-Allow-minibuffer-to-shrink-to-zero-lines.patch;
-  patch1 = ./patches/emacs-double-buffer.patch;
-  emacsNoGTK = (super.emacs25.overrideAttrs
-    (a : {
-      patches = a.patches ++ [patch0 patch1];
-    }))
-      .override { withGTK2 = false; withGTK3 = false;
-    srcRepo = true; };
-in emacsNoGTK;
+    (super.emacs25.overrideAttrs
+    (a :
+      {
+        patches = a.patches ++ [
+#          ./patches/0001-Allow-minibuffer-to-shrink-to-zero-lines.patch
+          ./patches/emacs-double-buffer.patch
+        ];
+      })).override { withGTK2 = false; withGTK3 = false; srcRepo = true; };
 
   mplayer = super.mplayer.override {
     x264Support = true;
     x264 = self.x264;
   };
 
+  compton = super.compton.overrideAttrs
+    (_ :
+    {
+      src =
+        super.fetchgit {
+        url = "https://github.com/larkery/compton.git";
+        rev = "3a28338cd8bd51188dbf000bfdf9404502a26ac8";
+        sha256 = "07lyw2df9cjcjmjjv1j70m1j4k8r9hbqivxb2vp4fl8zrxb2rq38";
+      };
+    });
 
   emacs-pdf-tools = (super.callPackage ./pdf-tools.nix {});
 
