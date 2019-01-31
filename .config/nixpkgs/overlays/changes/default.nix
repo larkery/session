1145f5bcaa82b59e4fca3c69d6e76f895d824471
Tom Hinton <tom.hinton@cse.org.uk>
2019-01-31 15:35:38 +0000

    theme blah

--- .config/nixpkgs/overlays/changes/default.nix
+++ .config/nixpkgs/overlays/changes/default.nix
@@ -1,18 +1,11 @@
 self: super:
 
 {
-  mplayer = super.mplayer.override {
-    x264Support = true;
-    x264 = self.x264;
-  };
-
   ripright = (super.callPackage ./ripright.nix {});
 
   xcopy = (super.callPackage ./xcopy.nix {});
 
-  xwinmosaic = (super.xwinmosaic.overrideAttrs (a : {
-    patches = [./patches/xwinmosaic-print-id.patch];
-  }));
-
   i3pop = (super.callPackage ./i3pop.nix {});
+
+  #qgis = (super.callPackage ./qgis.nix {});
 }
